d6f6fec74054a1d6224f72ac85e04e7a347d02c1
svenjaklenke <svenja.klenke@gmx.de>
2023-04-21 13:01:05 +0200

    add methods and tests

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -3,6 +3,8 @@ package de.neuefische;
 /*
 Überprüfe erst die Länge des Passworts und
 dann, ob Zahlen enthalten sind
+Checke, ob kleine/große Buchstaben enthalten sind
+Verbiete "schlechte" Passwörter wie "passwort" oder "123456"
  */
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +13,44 @@ public class Main {
 
     public static boolean laengePruefen(String a) {
         return a.length() >= 8;
+    }
+
+    public static boolean zahlPruefen(String b) {
+        return b.matches(".*[0-9].*");
+
+    }
+
+    public static boolean beidesPruefen(String c) {
+        return laengePruefen(c) && zahlPruefen(c);
+    }
+
+    public static boolean kleineBuchstaben(String d) {
+        return d.matches(".*[a-z].*");
+    }
+
+    public static boolean grosseBuchstaben(String e) {
+        return e.matches(".*[A-Z].*");
+    }
+
+    public static boolean beidesBuchstaben(String f) {
+        return kleineBuchstaben(f) && grosseBuchstaben(f);
+    }
+
+    public static boolean beidesBeides(String g) {
+        return beidesPruefen(g) && beidesBuchstaben(g);
+    }
+
+    public static boolean schlechtesPasswort(String h) {
+        String [] passwortArray = {"Passwort12345","Passwort1234","Passwort123","Passwort12","Passwort1"};
+        for (int i =0; i<passwortArray.length; i++){
+            if (h.equals(passwortArray[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean allesPruefen(String j) {
+        return beidesBeides(j) && !schlechtesPasswort(j);
     }
 }
--- src/test/java/de/neuefische/MainTest.java
+++ src/test/java/de/neuefische/MainTest.java
@@ -35,4 +35,203 @@ class MainTest {
         //THEN
         assertTrue(actual);
     }
+
+    @Test
+    void zahlPruefen_svenjakl1() {
+        //GIVEN
+        String a = "svenjakl1";
+        //WHEN
+        boolean actual = Main.zahlPruefen(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void zahlPruefen_svenjakl() {
+        //GIVEN
+        String a = "svenjakl";
+        //WHEN
+        boolean actual = Main.zahlPruefen(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void beidesPruefen_svenjakl22() {
+        //GIVEN
+        String a = "svenjakl22";
+        //WHEN
+        boolean actual = Main.beidesPruefen(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void beidesPruefen_svenjakl() {
+        //GIVEN
+        String a = "svenjakl";
+        //WHEN
+        boolean actual = Main.beidesPruefen(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void kleineBuchstaben_svenjakl() {
+        //GIVEN
+        String a = "svenjakl";
+        //WHEN
+        boolean actual = Main.kleineBuchstaben(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void kleineBuchstaben_SVENJAKL() {
+        //GIVEN
+        String a = "SVENJAKL";
+        //WHEN
+        boolean actual = Main.kleineBuchstaben(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void kleineBuchstaben_SVENJAkl() {
+        //GIVEN
+        String a = "SVENJAkl";
+        //WHEN
+        boolean actual = Main.grosseBuchstaben(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void grosseBuchstaben_SVENJAKL() {
+        //GIVEN
+        String a = "SVENJAKL";
+        //WHEN
+        boolean actual = Main.grosseBuchstaben(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void grosseBuchstaben_svenjakl() {
+        //GIVEN
+        String a = "svenjakl";
+        //WHEN
+        boolean actual = Main.grosseBuchstaben(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void grosseBuchstaben_svenjaKL() {
+        //GIVEN
+        String a = "svenjaKL";
+        //WHEN
+        boolean actual = Main.grosseBuchstaben(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void beidesBuchstaben_svenjaKL() {
+        //GIVEN
+        String a = "svenjaKL";
+        //WHEN
+        boolean actual = Main.beidesBuchstaben(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void beidesBuchstaben_svenjakl() {
+        //GIVEN
+        String a = "svenjakl";
+        //WHEN
+        boolean actual = Main.beidesBuchstaben(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void beidesBuchstaben_SVENJAKL() {
+        //GIVEN
+        String a = "SVENJAKL";
+        //WHEN
+        boolean actual = Main.beidesBuchstaben(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void beidesBeides_svenjaKL1() {
+        //GIVEN
+        String a = "svenjaKL1";
+        //WHEN
+        boolean actual = Main.beidesBeides(a);
+        //THEN
+        assertTrue(actual);
+    }
+    @Test
+    void beidesBeides_svenjakl1() {
+        //GIVEN
+        String a = "svenjakl1";
+        //WHEN
+        boolean actual = Main.beidesBeides(a);
+        //THEN
+        assertFalse(actual);
+    }
+    @Test
+    void beidesBeides_svenjaKL() {
+        //GIVEN
+        String a = "svenjaKL";
+        //WHEN
+        boolean actual = Main.beidesBeides(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void schlechtesPasswort_Passwort1234() {
+        //GIVEN
+        String a = "Passwort1234";
+        //WHEN
+        boolean actual = Main.schlechtesPasswort(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void schlechtesPasswort_SvenjaKL123() {
+        //GIVEN
+        String a = "SvenjaKL123";
+        //WHEN
+        boolean actual = Main.schlechtesPasswort(a);
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    void allesPruefen_SvenjaKL123() {
+        //GIVEN
+        String a = "SvenjaKL123";
+        //WHEN
+        boolean actual = Main.allesPruefen(a);
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    void allesPruefen_Passwort123() {
+        //GIVEN
+        String a = "Passwort123";
+        //WHEN
+        boolean actual = Main.allesPruefen(a);
+        //THEN
+        assertFalse(actual);
+    }
+
 }
